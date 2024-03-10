1ff67135c8635cbdd94229f7f66bfd95f388a1f8
Spencer Thayer <me@spencerthayer.com>
2024-03-10 13:45:33 -0700

    update polls.py

--- polls.py
+++ polls.py
@@ -130,7 +130,7 @@ def calculate_and_print_differential(df, period_value, period_type='months', per
             * filtered_df['sample_size_weight'] \
             * filtered_df['population_weight'] \
             * filtered_df['partisan_weight'] \
-            * filtered_df['state_rank'] 
+            * filtered_df['state_rank']
 
         weighted_sums = filtered_df.groupby('candidate_name')['combined_weight'].apply(lambda x: (x * filtered_df.loc[x.index, 'pct']).sum())
         total_weights = filtered_df.groupby('candidate_name')['combined_weight'].sum()
